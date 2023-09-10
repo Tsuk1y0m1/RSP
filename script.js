187a468d737696be8e51f4a640c9e8c4c786192a
Polina <shapovalova2006p@gmail.com>
2023-09-10 21:05:51 +1000

    modified:css,html. game is connected to site

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 // rock paper scissors
+const btnStart = document.querySelector(".start")
+const player = document.querySelector(".player")
+const computer = document.querySelector(".computer")
+const choices = document.querySelectorAll(".choice")
+
 
 const getComputerChoice = () => {
     let result = Math.floor(Math.random() * 3)
@@ -16,38 +21,40 @@ const getComputerChoice = () => {
     return result
 }
 
-const checkAnswer = (playerChoice) => {
-    playerChoice = playerChoice.toLowerCase()
-    return (
-        playerChoice == "rock" ||
-        playerChoice == "scissors" ||
-        playerChoice == "paper") ? true : false;
-}
+
 
 const playRound = (playerChoice) => {
-    if (checkAnswer(playerChoice)) {
-        playerChoice = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1).toLowerCase();
-
-        const computerChoice = getComputerChoice()
-
-        let result = (playerChoice == computerChoice) ? `${playerChoice} == ${computerChoice}. No one win.` :
+    let computerChoice = getComputerChoice()
+    let result = (playerChoice == computerChoice) ? [0, 0] :
             (playerChoice == "Rock" && computerChoice == "Scissors" ||
                 playerChoice == "Scissors" && computerChoice == "Paper" ||
-                playerChoice == "Paper" && computerChoice == "Rock") ? `You win! ${playerChoice} beats ${computerChoice}` :
-                `You lose!${computerChoice} beats ${playerChoice}`;
+                playerChoice == "Paper" && computerChoice == "Rock") ? [1, 0] :
+                [0, 1];
 
         return result
-    } else {
-        return "You wrote smt wrong. Repeat"
-    }
 }
 
-const startPlay = () => {
-    alert('You will have 5 rounds! Let\'s start!')
-    for (let i = 0; i <= 5; i++) {
-        let playerAnswer = prompt("Write rock || paper || scissors")
-        console.log(playRound(playerAnswer))
+const gamePlay = (e) => {
+        const playerChoice = e.target.alt
+        if (!playerChoice) return
+        let result = playRound(playerChoice)
+        let count = [player.textContent, computer.textContent]
+        count[0] = +count.at(0) + result.at(0)
+        count[1] = +count.at(1) + result.at(1)
+        player.textContent = count.at(0)
+        computer.textContent = count.at(1)
+        if (count[0] == 5) {
+            alert('You win!')
+            startPlayBtn()   
+        } else if (count[1] == 5){
+            alert('Bob win!')
+            startPlayBtn()   
+        }
+        
     }
+const startPlayBtn = () => {
+    player.textContent = "0"
+    computer.textContent = "0"
 }
-
-
+choices.forEach(choice => choice.addEventListener('click', gamePlay))
+btnStart.addEventListener('click', startPlayBtn)
